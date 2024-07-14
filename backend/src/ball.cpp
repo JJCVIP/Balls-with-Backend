36143676fef0f50e25c410e00b587da47321af07
J.J. <88807892+JJCVIP@users.noreply.github.com>
2024-07-14 15:59:59 +0000

    Improved Ball Bouncing

--- backend/src/ball.cpp
+++ backend/src/ball.cpp
@@ -12,8 +12,8 @@ std::array<double,2> Ball::getPosition() const{
 void Ball::update(const double time_delta, const std::array<short,2>& bounds){
     //Gravity 
     //Reminder positive number is down
-    constexpr double g = 9.81;
-    const double acceleration_of_gravity = g * (time_delta) * (time_delta);
+    constexpr double g = 98.1;
+    const double acceleration_of_gravity =  g * (time_delta);
 
     velocity[1] += acceleration_of_gravity;
     position[1] += velocity[1] * time_delta;
@@ -22,7 +22,7 @@ void Ball::update(const double time_delta, const std::array<short,2>& bounds){
 
     //clamp
     position[0] = std::clamp(short(position[0]), short{0}, bounds[0]);
-    position[1] = std::clamp(short(position[1]), short{0}, bounds[1]);
+    position[1] = std::clamp(short(position[1]), short{-32000}, bounds[1]);
 
     if(position[1] == double(bounds[1])){
         velocity[1] = -1 * velocity[1] * 0.9;
